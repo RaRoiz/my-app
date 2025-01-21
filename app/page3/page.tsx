fdd4caf12aee10afe55a29a98df6791da721319d
RaRoiz <tataphon32@gmail.com>
2025-01-21 10:29:00 +0700

    page

---
+++ app/page3/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import Link from "next/link";
+export default function Page() {
+  return (
+    <div>
+      <h1 className = "text-3xl text-center" > Page1 </h1>
+
+      <Image src="/250144965.jpg"width={300} height={300} alt={"player"}/>
+
+      <Link href="\page1">Go to Page1</Link>
+      <Link href="\page2">Go to Page2</Link>
+      <Link href="\page3">Go to Page3</Link>
+    </div>
+  );
+}
